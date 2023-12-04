ccc7aa9120d6cb26d462c3890ec470a43c157da2
Srinivas Borigarla <iamborigarla@Srinivass-MacBook-Pro.local>
2023-12-04 19:45:51 +0530

    UI upgraded

--- src/App.js
+++ src/App.js
@@ -3,53 +3,82 @@ import './App.css';
 import MyNavbar from './navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Container, Row, Col, Button, DropdownButton, Dropdown, ButtonGroup, Card  } from "react-bootstrap";
+import { Container, Row, Col, Button, Card  } from "react-bootstrap";
 import React, { useState } from "react";
 
 import { ethers }  from "ethers";
-const provider = new ethers.providers.Web3Provider(window.ethereum)
-await provider.send("eth_requestAccounts", []);
-const signer = provider.getSigner()
+
+// connect to metamask
+// async function ConnectToMetamask () {
+//   const [address, setDefaultAddress] = useState('');
+//   const [provider, setDefaultProvider] = useState('');
+//   const [signer, setDefaultSigner] = useState('');
+  
+//   try {
+//     // Check if metamask installed
+//     if (window.ethereum) {
+//       //Request account access if not already authorized
+//       const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+//       const provider = new ethers.providers.Web3Provider(window.ethereum)
+//       await provider.send("eth_requestAccounts", []);
+//       const signer = provider.getSigner();
+//       const address = await signer.getAddress();
+
+//       setDefaultAddress(address);
+//     } else {
+//       console.error('MetaMask is not installed');
+//     }
+//   } catch (error) {
+//     console.error(error);
+//   }
+//   return [provider, signer, address];
+// };
+
+
+
+
+// async function myname() {
+//   const name = await signercontract.issueCredits("0xA03ab8a9d99F3779085BCc528b35EaCD949aeC1e", '1000000000000000000');
+// console.log(name);
+// }
+
+// myname()
 
 
 function App() {
-  const [project, setProject] = useState("Select project");
-  const [token, setToken] = useState("Total number of tokens available");
-  const [carb, setCarb] = useState("Enter total tokens to buy");
   const [benf, setBenf] = useState("Enter name of the beneficiary"); 
-  const [add, setAdd] = useState("Enter wallet address"); 
-  const [usdc, setUsdc] = useState("Total cost in USDC (Minimum 1USDC)"); 
+  const [address, setAdd] = useState("Enter wallet address"); 
+  const [token, setToken] = useState("Enter total tokens to buy");
   
-  if (carb==='') setCarb('Enter total tokens to buy')
-  if (benf==='') setBenf('Enter name of the beneficiary')
-  if (add==='') setAdd('Enter wallet address')
+  if (benf==='') setBenf('Enter name of the beneficiary');
+  if (address==='') setAdd('Enter wallet address');
+  if (token==='') setToken('Enter total tokens to buy');
 
-  const handleTokenChange = (event) => {
-    setToken(event.target.value);
-  };
-  const handleCarbonChange = (event) => {
-    setCarb(event.target.value);
-    const val = Math.round(event.target.value*(120/100),2)
-    setUsdc('Total cost in USDC (Minimum 1USDC) :- '+val);
-  };
+  const accounts = window.ethereum.request({ method: 'eth_requestAccounts' });
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  provider.send("eth_requestAccounts", []);
+  const signer = provider.getSigner();
+
+  const tokbuyabi = [
+    "function issueCredits(address to, uint256 amount)",
+    "function redeemCredits(uint256 amount)",
+  ]
+  const tokAddress = "0xDC373f0D2a019109285bb315b12075190763fCB3";
+  const tokcontract = new ethers.Contract(tokAddress, tokbuyabi, provider);
+  const signercontract = new ethers.Contract(tokAddress, tokbuyabi, signer);
+  
   const handleBenfChange = (event) => {
     setBenf(event.target.value);
   };
   const handleAddressChange = (event) => {
     setAdd(event.target.value);
   };
-  const handleUSDCChange = (event) => {
-    setUsdc(event.target.value);
+  const handleTokenChange = (event) => {
+    setToken(event.target.value);
   };
-  const handleProject = (event) => {
-    setProject(event)
-    console.log(event)
-    if (event==='400 MW Solar Power Project at Bhadla, Rajasthan (GS7071)')
-    {
-      setToken('Total number of tokens available :- 100');
-    }
-    console.log(token)
-  };
+  const handleClick = () => {
+    const assignCredits = signercontract.issueCredits(address, token);
+  }
   return (
     <div>
       <MyNavbar/>
@@ -57,42 +86,24 @@ function App() {
     <Container>
       <Row>
       <Col md={6} style={{ border: "1px solid black", padding: "20px"}}>
-              <div className="d-flex flex-column justify-content-between">
+          <div className="d-flex flex-column justify-content-between">
+            <input className="text-center"  placeholder={benf}
+            style={{marginBottom: "10px",borderWidth:'1.3px', color: '#232323', borderColor:'#232323', borderRadius: '3px'}}
+            onChange={handleBenfChange}/> 
 
-              <DropdownButton 
-              as={ButtonGroup}
-              variant="secondary"
-              id="dropdown-menu-align-responsive-1"
-              style={{marginBottom: "10px",borderWidth:'1.3px', color: '#232323', borderColor:'#232323', borderRadius: '3px'}}
-              title={project} onSelect={handleProject}>
-                <Dropdown.Item eventKey='400 MW Solar Power Project at Bhadla, Rajasthan (GS7071)'>400 MW Solar Power Project at Bhadla, Rajasthan (GS7071)</Dropdown.Item>
-              </DropdownButton>
+            <input className="text-center" placeholder={address} 
+            style={{marginBottom: "10px",borderWidth:'1.3px', color: '#232323', borderColor:'#232323', borderRadius: '3px'}}
+            onChange={handleAddressChange}/>
 
-                <input className="text-center" value={token} 
-                style={{marginBottom: "10px",borderWidth:'1.3px', color: '#232323', borderColor:'#232323', borderRadius: '3px'}}
-                onChange={handleTokenChange} disabled />
-
-                <input className="text-center" placeholder={carb} 
-                style={{marginBottom: "10px",borderWidth:'1.3px', color: '#232323', borderColor:'#232323', borderRadius: '3px'}}
-                onChange={handleCarbonChange}/>
-
-                <input className="text-center"  placeholder={benf}
-                style={{marginBottom: "10px",borderWidth:'1.3px', color: '#232323', borderColor:'#232323', borderRadius: '3px'}}
-                onChange={handleBenfChange}/> 
-
-                <input className="text-center" placeholder={add} 
-                style={{marginBottom: "10px",borderWidth:'1.3px', color: '#232323', borderColor:'#232323', borderRadius: '3px'}}
-                onChange={handleAddressChange}/> 
-
-                <input className="text-center" value={usdc} 
-                style={{marginBottom: "10px",borderWidth:'1.3px', color: '#232323', borderColor:'#232323', borderRadius: '3px'}}
-                onChange={handleUSDCChange} disabled />
-                </div>
-                </Col>
+            <input className="text-center" placeholder={token} 
+            style={{marginBottom: "10px",borderWidth:'1.3px', color: '#232323', borderColor:'#232323', borderRadius: '3px'}}
+            onChange={handleTokenChange}/>
+          <Button variant="secondary" size="sm" onClick={handleClick}>Reedem tokens</Button>
+            </div>
+            </Col>
       </Row>
       <Row>
         <Col md={1}>
-        <Card body>This is some text within a card body.</Card>;
         </Col>
       </Row>
     </Container>
